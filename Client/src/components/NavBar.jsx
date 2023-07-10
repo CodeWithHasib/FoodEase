d734beed5f851b47a29cfb809c5e65ac29d188ff
Hasib Hossain <boraborhasib@gmail.com>
2023-07-10 22:21:22 +0600

    Navbar added

--- Client/src/components/NavBar.jsx
+++ Client/src/components/NavBar.jsx
@@ -4,8 +4,8 @@ import { FiMenu } from 'react-icons/fi';
 const NavBar = () => {
   const [show, setShow] = useState(false)
   return (
-    <nav className=''>
-      <div className="md:px-20 px-7 flex justify-between items-center  py-2">
+    <nav className='fixed md:px-20 px-7 top-0 w-full bg-blue-300'>
+      <div className=" flex justify-between items-center  py-2">
         <div className="">
           <h1 className='text-3xl font-bold'>Food Ease</h1>
         </div>
@@ -22,7 +22,7 @@ const NavBar = () => {
             </li>
           </ul>
         </div>
-        <div className="">
+        <div className={`md:hidden`}>
           <FiMenu onClick={() => setShow(!show)} className='text-3xl' />
         </div>
       </div>
