34a4f7e652dda6d6b64cd305d41a7b2bcf8af223
Hasib Hossain <boraborhasib@gmail.com>
2023-07-11 00:18:55 +0600

    Small updated

--- Client/src/components/NavBar.jsx
+++ Client/src/components/NavBar.jsx
@@ -2,7 +2,12 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FiMenu } from 'react-icons/fi';
 const NavBar = () => {
-  const [show, setShow] = useState(false)
+  const [show, setShow] = useState(false);
+  const navLinks = [
+    { name: 'Home', link: '/' },
+    { name: 'Items', link: '/' },
+    { name: 'Shop', link: '/' },
+  ]
   return (
     <nav className='fixed  top-0 w-full bg-white'>
       <div className=" flex md:px-20 px-7 justify-between bg-blue-600 w-full items-center  backdrop-blur-2xl bg-opacity-20 py-4">
@@ -11,15 +16,18 @@ const NavBar = () => {
         </div>
         <div className="hidden md:block">
           <ul>
+            {
+
+              navLinks.map((link, index) => (
+                <li key={index} className='inline-block px-4 py-2'>
+                  <Link className='font-bold hover:text-blue-600 duration-200' to={link.link}>{link.name}</Link>
+                </li>
+              ))
+            }
             <li className='inline-block px-4 py-2'>
-              <Link className='font-bold hover:text-blue-600 duration-200' to='/'>Home</Link>
+              <Link className='font-bold hover:text-blue-600 duration-200' to='/login'>Login</Link>
             </li>
-            <li className='inline-block px-4 py-2'>
-              <Link className='font-bold hover:text-blue-600 duration-200' to='/'>Items</Link>
-            </li>
-            <li className='inline-block px-4 py-2'>
-              <Link className='font-bold hover:text-blue-600 duration-200' to='/'>Shop</Link>
-            </li>
+
           </ul>
         </div>
         <div className={`md:hidden`}>
