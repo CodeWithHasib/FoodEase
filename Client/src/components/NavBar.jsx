4b8edf580d716cdad8590c5faa6c0f070f761a41
Hasib Hossain <boraborhasib@gmail.com>
2023-07-10 22:13:32 +0600

    Navbar added

--- Client/src/components/NavBar.jsx
+++ Client/src/components/NavBar.jsx
@@ -4,26 +4,39 @@ import { FiMenu } from 'react-icons/fi';
 const NavBar = () => {
   const [show, setShow] = useState(false)
   return (
-    <nav className='md:px-20 px-7 flex justify-between items-center  py-2'>
-      <div className="">
-        <h1 className='text-3xl font-bold'>Food Ease</h1>
+    <nav className=''>
+      <div className="md:px-20 px-7 flex justify-between items-center  py-2">
+        <div className="">
+          <h1 className='text-3xl font-bold'>Food Ease</h1>
+        </div>
+        <div className="hidden md:block">
+          <ul>
+            <li className='inline-block px-4 py-2'>
+              <Link className='font-bold hover:text-blue-600 duration-200' to='/'>Home</Link>
+            </li>
+            <li className='inline-block px-4 py-2'>
+              <Link className='font-bold hover:text-blue-600 duration-200' to='/'>Items</Link>
+            </li>
+            <li className='inline-block px-4 py-2'>
+              <Link className='font-bold hover:text-blue-600 duration-200' to='/'>Shop</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="">
+          <FiMenu onClick={() => setShow(!show)} className='text-3xl' />
+        </div>
       </div>
-      <div className="hidden md:block">
-        <ul>
-          <li className='inline-block px-4 py-2'>
-            <Link className='font-bold hover:text-blue-600 duration-200' to='/'>Home</Link>
-          </li>
-          <li className='inline-block px-4 py-2'>
-            <Link className='font-bold hover:text-blue-600 duration-200' to='/'>Items</Link>
-          </li>
-          <li className='inline-block px-4 py-2'>
-            <Link className='font-bold hover:text-blue-600 duration-200' to='/'>Shop</Link>
-          </li>
-        </ul>
-      </div>
-      <div className="">
-        <FiMenu onClick={() => setShow(!show)} className='text-3xl' />
-      </div>
+      <ul className={`${show ? 'mt-5' : '-mt-28'} md:block bg-slate-200`}>
+        <li className='inline-block px-4 py-2'>
+          <Link className='font-bold hover:text-blue-600 duration-200' to='/'>Home</Link>
+        </li>
+        <li className='inline-block px-4 py-2'>
+          <Link className='font-bold hover:text-blue-600 duration-200' to='/'>Items</Link>
+        </li>
+        <li className='inline-block px-4 py-2'>
+          <Link className='font-bold hover:text-blue-600 duration-200' to='/'>Shop</Link>
+        </li>
+      </ul>
     </nav>
   );
 };
--- Client/src/pages/Home/Home.jsx
+++ Client/src/pages/Home/Home.jsx
@@ -4,7 +4,10 @@ import NavBar from '../../components/NavBar';
 const Home = () => {
     return (
         <div>
-            <NavBar /> 
+            <NavBar />
+            <div className="bg-slate-500 mt-28 h-12 w-full">
+                <h1>We belive in Allah </h1>
+            </div>
         </div>
     );
 };
