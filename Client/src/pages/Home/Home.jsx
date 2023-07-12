f73ae960e3bd9ec0acfa0f9229be7c23aad70119
Hasib Hossain <boraborhasib@gmail.com>
2023-07-12 21:10:38 +0600

    Small updated

--- Client/src/pages/Home/Home.jsx
+++ Client/src/pages/Home/Home.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import NavBar from '../../components/NavBar';
 import Cards from './Cards';
+import Hero from './Hero';
 
 const Home = () => {
     return (
-        <div>
-            <div className="max-w-screen-xl w-[90%] mx-auto">
-                <Cards />
-            </div>
+        <div className="max-w-screen-xl w-[90%] mx-auto">
+            <Hero />
+            <Cards />
         </div>
     );
 };
